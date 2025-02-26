fa8830887fc6037cda697c75052e9ca5072ff81d
j <j>
2025-02-26 21:41:39 +0100

    day_1 checked

--- fondamentaux_etl/main.py
+++ fondamentaux_etl/main.py
@@ -1,26 +1,55 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import sum
 
-spark = SparkSession \
+
+# 1 --> Exctract : Read csv
+def extract(spark: SparkSession, input_file_path: str):
+    """Extract data from any csv source"""
+    try:
+        return spark.read.csv(
+            input_file_path, 
+            header=True, 
+            inferSchema=True
+            )
+    except Exception as e:
+        print(f"Error {e}")
+
+
+# 2 --> Tranform : Calcule le total des ventes par produit
+def transform(df):
+    """Compute total unit sold for a product"""
+    try:
+        return df.groupBy("product_id").agg(sum("quantity").alias('total_unit_sold'))
+    except Exception as e:
+        print(f"Error {e}")
+
+# 3 --> Output en parquet
+def load(df, outpout_file_path: str):
+    """Load data into  parquet file"""
+    df.write.mode("overwrite") \
+    .parquet(
+        outpout_file_path, 
+        compression="snappy"
+        )
+
+def main():
+    spark = SparkSession \
     .builder \
     .appName("etl") \
     .getOrCreate()
 
-# 1 --> Exctract : Read csv
-df_sales = spark.read.csv('/Users/thomasrivieres/clean-data-pipelines/.data/sales.csv', 
-                          header=True, inferSchema=True)
+    try:
+        input_file_path= "/Users/thomasrivieres/clean-data-pipelines/.data/sales.csv"
+        outpout_file_path= "/Users/thomasrivieres/clean-data-pipelines/.data/result.parquet"
 
-# 2 --> Tranfoorm : Calcule le total des ventes par produit
-result_df = df_sales.groupBy("product_id").agg(sum("quantity").alias('total_unit_sold'))
+        raw_df = extract(spark, input_file_path)
+        df_transformed = transform(raw_df)
+        load(df_transformed, outpout_file_path)
 
-# 3 --> Output en parquet
-result_df.write.parquet("/Users/thomasrivieres/clean-data-pipelines/.data/result.parquet", compression="snappy")
-
-df_parquet = spark.read.parquet("/Users/thomasrivieres/clean-data-pipelines/.data/result.parquet")
-df_parquet.show()
+    except Exception as e:
+        print(f"Error {e}")
 
 
 
 if __name__ == "__main__":
-    pass
-    
+    main()    
